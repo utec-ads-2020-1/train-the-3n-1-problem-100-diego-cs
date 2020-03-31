b2fbacab0390dd8305708260e5a461bb7a02e047
Diego Sebastian Enciso Lozano <diego.enciso@utec.edu.pe>
2020-03-31 17:30:00 -0500

    main.cpp updated

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 /* compile -> g++ main.cpp -o exec*/
 /* run     -> ./exec < input */
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char *argv[]) {
 
     std::string line;  
-    while(getline(std::cin, line))
+    while(getline(std::cin, line))     
         max_cycle(line);
-
-    return EXIT_SUCCESS;
+   
+    return 0;
 }
