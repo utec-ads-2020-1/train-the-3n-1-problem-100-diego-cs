687e84c66bc963dcf17e43a7185b8d0e087fbfc1
Diego Sebastian Enciso Lozano <diego.enciso@utec.edu.pe>
2020-03-31 19:35:10 -0500

    solution with scanf

--- main.cpp
+++ main.cpp
@@ -1,13 +1,29 @@
-#include "maxcycle.hpp"
+#include <iostream>
+#include <chrono>
 
-/* compile -> g++ main.cpp -o exec*/
-/* run     -> ./exec < input */
+int cycle(int n) {
+    if (n == 1) return 1;
+    return cycle(n % 2 != 0 ? 3 * n + 1 : n / 2) + 1;
+}
+
+void max_cycle(int a, int b) {
+    while ((scanf("%d %d", &a, &b)) == 2) {
+        int i = a;
+        int j = b;
+        if (i > j) std::swap(i, j);
+        int max = cycle(i);
+        for (i; i <= j; ++i)
+            if (cycle(i) > max)
+                max = cycle(i);
+
+        std::cout << a << " " << b << " " << max << std::endl;
+    }
+}
 
 int main(int argc, char *argv[]) {
 
-    std::string line;  
-    while(getline(std::cin, line))     
-        max_cycle(line);
+    int a, b;
+    max_cycle(a, b);
    
     return 0;
 }
